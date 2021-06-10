bd28e026d10dcc808a1df85c8a46876989044de1
Samuel Sherrer <samuel-sherrer@hotmail.com>
2021-06-10 19:25:26 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Vue-shop
+
+## Description
+
+This code was based on this course: [Vue.js 3 Essential Training](https://www.linkedin.com/learning/vue-js-3-essential-training/what-you-should-learn-about-vue-js).
+If you wanna learn VueJS, I really recommend this course to you.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="container">
+    <range-selector :products="filteredProducts" v-model="max" />
+    <product-list :products="filteredProducts" />
+  </section>
+</template>
+
+<script>
+import RangeSelector from "@/components/RangeSelector"
+import ProductList from "@/components/ProductList"
+
+export default {
+  name: "Home",
+  data: function () {
+    return {
+      max: 50,
+      displayCart: false
+    }
+  },
+  props: ["products"],
+  components: {
+    RangeSelector,
+    ProductList
+  },
+  computed: {
+    filteredProducts() {
+      return this.products.filter(item => item.price < Number(this.max))
+    }
+  }
+}
+</script>
